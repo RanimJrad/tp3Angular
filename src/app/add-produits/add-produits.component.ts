2a47f95c9351f8c0e907794901761b46deb16609
RanimJrad <Ranimjrad0@gmail.com>
2023-10-28 13:13:01 +0200

    Angular

--- src/app/add-produits/add-produits.component.ts
+++ src/app/add-produits/add-produits.component.ts
@@ -2,6 +2,7 @@ import { Component,OnInit } from '@angular/core';
 import { Produit } from '../model/produit.model';
 import { ProduitService } from '../services/produit.service';
 import { Router } from '@angular/router';
+import { Categorie } from '../model/categorie.model';
 
 
 @Component({
@@ -11,16 +12,22 @@ import { Router } from '@angular/router';
 })
 export class AddProduitsComponent implements OnInit{
   newProduit= new Produit();
-  message : string ='' ;
+  categories! : Categorie[];
+  newIdCat! : number; 
+  newCategorie! : Categorie;
   
   constructor(private ProduitService : ProduitService ,
               private router :Router,){
   }
   ngOnInit() : void{
+    this.categories = this.ProduitService.listeCategories();
+
     
   }
   addProduit(){
-    //console.log(this.newProduit);
+    //console.log(this.newIdCat);
+    this.newCategorie = this.ProduitService.consulterCategorie(this.newIdCat);
+    this.newProduit.categorie = this.newCategorie;
     this.ProduitService.ajouterProduit(this.newProduit);
     this.router.navigate(['produits']);
 
--- src/app/services/produit.service.ts
+++ src/app/services/produit.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { Produit } from '../model/produit.model';
+import { Categorie } from '../model/categorie.model';
 
 @Injectable({
   providedIn: 'root'
@@ -7,12 +8,14 @@ import { Produit } from '../model/produit.model';
 export class ProduitService {
   produits : Produit[];
   produit= new Produit;
+  categories : Categorie[];
   constructor() {
-    console.log("creation du service produit !");
+    this.categories=[{idCat : 1, nomCat : "PC"},
+                      {idCat : 2, nomCat : "Imprimante"}];
     this.produits = [
-      {idProduit : 1, nomProduit : "PC Asus", prixProduit : 3000.600, dateCreation : new Date("01/14/2011")},
-      {idProduit : 2, nomProduit : "Imprimante Epson", prixProduit : 450, dateCreation : new Date("12/17/2010")},
-      {idProduit : 3, nomProduit :"Tablette Samsung", prixProduit : 900.123, dateCreation : new Date("02/20/2020")}
+      {idProduit : 1, nomProduit : "PC Asus", prixProduit : 3000.600, dateCreation : new Date("01/14/2011"), categorie : {idCat : 1, nomCat : "PC"}},
+      {idProduit : 2, nomProduit : "Imprimante Epson", prixProduit : 450, dateCreation : new Date("12/17/2010"), categorie :{idCat : 2, nomCat : "Imprimante"}},
+      {idProduit : 3, nomProduit :"Tablette Samsung", prixProduit : 900.123, dateCreation : new Date("02/20/2020"), categorie : {idCat : 1, nomCat : "PC"}}
       ];
 
    }
@@ -62,4 +65,13 @@ export class ProduitService {
       this.ajouterProduit(p);
       this.trierProduits();
     }
+
+    listeCategories():Categorie[] {
+      return this.categories;
+    }
+
+    consulterCategorie(id:number): Categorie{
+      return this.categories.find(cat => cat.idCat == id)!;
+    }
+
 }
